29c4fb156521efeaa0bdc325e9e6dc125a1ab5a0
choisungwook <kgg1959@naver.com>
2021-07-30 19:28:00 +0900

    프로젝트 생성

---
+++ src/test/java/com/demo/jasypt/jasypt/JasyptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jasypt.jasypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
